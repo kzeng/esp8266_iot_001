9222b149445c417d9f0d808fd6f8bf5f2bf58fa5
zengkai001 <zengkai001@qq.com>
2024-05-23 23:27:30 +0800

    mysql

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ import json
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # Replace with your database URI
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # Replace with your database URI
+# db = SQLAlchemy(app)
+
+## app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@hostname/database'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:zk010823@118.31.36.131/esp8266-xds'
 db = SQLAlchemy(app)
 
-
-g_dashboard_get_info1_dict = {'system_time': '0000年00月00日 星期五 00:00', 'device_id': '00', 'run_status': "<i class='fa fa-circle red'> 运行中", ' run_time': '243', 'record_interval': '1', 'update_time': '更新于: 0000-0-0 00:00:00', 'temp_1': '35.4', 'hum_1': '61.7',  'control_mode_1': "自动 <i class='glyphicon glyphicon-transfer'></i>", 'control_method_1': "节能 <i class='glyphicon gl yphicon-leaf'></i>", 'temp_status_info_1': "制冷 <i class='glyphicon glyphicon-asterisk'></i>", 'humi_status_info_1': " 加湿 <i class='glyphicon glyphicon-tint'></i>", 'target_temp_1': '16.0', 'control_temp_1': '0.5', 'target_hum_1': '90.0' , 'control_hum_1': '9.0', 'temp_2': '--', 'hum_2': '--', 'control_mode_2': 'hide', 'control_method_2': 'hide', 'temp_sta tus_info_2': '', 'humi_status_info_2': '', 'target_temp_2': '20.0', 'control_temp_2': '1.0', 'target_hum_2': '96.0', 'co ntrol_hum_2': '5.0', 'temp_3': '36.0', 'hum_3': '51.0', 'control_mode_3': "自动 <i class='glyphicon glyphicon-transfer'> </i>", 'control_method_3': 'hide', 'temp_status_info_3': "制冷 <i class='glyphicon glyphicon-asterisk'></i>", 'humi_stat us_info_3': "加湿 <i class='glyphicon glyphicon-tint'></i>", 'target_temp_3': '20.0', 'control_temp_3': '1.0', 'target_h um_3': '96.0', 'control_hum_3': '5.0', 'temp_4': '--', 'hum_4': '--', 'control_mode_4': 'hide', 'control_method_4': 'hid e', 'temp_status_info_4': '', 'humi_status_info_4': '', 'target_temp_4': '20.0', 'control_temp_4': '3.0', 'target_hum_4' : '97.8', 'control_hum_4': '5.1', 'temp_5': '35.7', 'hum_5': '56.3', 'control_mode_5': "自动 <i class='glyphicon glyphic on-transfer'></i>", 'control_method_5': "节能 <i class='glyphicon glyphicon-leaf'></i>", 'accm_1': "<i class='fa fa-circ le accm1'></i>", 'accm_2': "<i class='fa fa-circle accm2'></i>", 'accm_3': "<i class='fa fa-circle accm2'></i>", 'accm_4 ': "<i class='fa fa-circle accm2'></i>", 'temp_status_info_5': "制冷 <i class='glyphicon glyphicon-asterisk'></i>", 'hum i_status_info_5': "加湿 <i class='glyphicon glyphicon-tint'></i>", 'target_temp_5': '16.0', 'control_temp_5': '0.5', 'target_hum_5': '90.0', 'control_hum_5': '9.0'}
-
+g_dashboard_get_info1_dict = {'system_time': '0000年00月00日 星期五 00:00', 'device_id': '00', 'run_status': "<i class='fa fa-circle red'> 运行中", ' run_time': '243', 'record_interval': '1', 'update_time': '更新于: 0000-0-0 00:00:00', 'temp_1': '35.4', 'hum_1': '61.7',  'control_mode_1': "自动 ", 'control_method_1': "节能 ", 'temp_status_info_1': "制冷 ", 'humi_status_info_1': " 加湿 ", 'target_temp_1': '16.0', 'control_temp_1': '0.5', 'target_hum_1': '90.0' , 'control_hum_1': '9.0', 'temp_2': '--', 'hum_2': '--', 'control_mode_2': 'hide', 'control_method_2': 'hide', 'temp_sta tus_info_2': '', 'humi_status_info_2': '', 'target_temp_2': '20.0', 'control_temp_2': '1.0', 'target_hum_2': '96.0', 'co ntrol_hum_2': '5.0', 'temp_3': '36.0', 'hum_3': '51.0', 'control_mode_3': "自动 ", 'control_method_3': 'hide', 'temp_status_info_3': "制冷 ", 'humi_stat us_info_3': "加湿 ", 'target_temp_3': '20.0', 'control_temp_3': '1.0', 'target_h um_3': '96.0', 'control_hum_3': '5.0', 'temp_4': '--', 'hum_4': '--', 'control_mode_4': 'hide', 'control_method_4': 'hid e', 'temp_status_info_4': '', 'humi_status_info_4': '', 'target_temp_4': '20.0', 'control_temp_4': '3.0', 'target_hum_4' : '97.8', 'control_hum_4': '5.1', 'temp_5': '35.7', 'hum_5': '56.3', 'control_mode_5': "自动 ", 'control_method_5': "节能 ", 'accm_1': "<i class='fa fa-circ le accm1'></i>", 'accm_2': "<i class='fa fa-circle accm2'></i>", 'accm_3': "<i class='fa fa-circle accm2'></i>", 'accm_4 ': "<i class='fa fa-circle accm2'></i>", 'temp_status_info_5': "制冷 ", 'hum i_status_info_5': "加湿 ", 'target_temp_5': '16.0', 'control_temp_5': '0.5', 'target_hum_5': '90.0', 'control_hum_5': '9.0'}
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,14 +26,61 @@ class InstData(db.Model):
     # api_key = db.Column(db.String(100))
     inst_name = db.Column(db.String(100))
     sensor_name = db.Column(db.String(100))
+
     temp1 = db.Column(db.String(32))
     humidity1 = db.Column(db.String(32))
+    target_temp1 = db.Column(db.String(32))
+    target_hum1 = db.Column(db.String(32))
+    control_temp1 = db.Column(db.String(32))
+    control_hum1 = db.Column(db.String(32))
+    control_mode1 = db.Column(db.String(32))
+    control_method1 = db.Column(db.String(32))
+    temp_status_info1 = db.Column(db.String(32))
+    humi_status_info1 = db.Column(db.String(32))
+
     temp2 = db.Column(db.String(32))
     humidity2 = db.Column(db.String(32))
+    target_temp2 = db.Column(db.String(32))
+    target_hum2 = db.Column(db.String(32))
+    control_temp2 = db.Column(db.String(32))
+    control_hum2 = db.Column(db.String(32))
+    control_mode2 = db.Column(db.String(32))
+    control_method2 = db.Column(db.String(32))
+    temp_status_info2 = db.Column(db.String(32))
+    humi_status_info2 = db.Column(db.String(32))
+    
     temp3 = db.Column(db.String(32))
     humidity3 = db.Column(db.String(32))
+    target_temp3 = db.Column(db.String(32))
+    target_hum3 = db.Column(db.String(32))
+    control_temp3 = db.Column(db.String(32))
+    control_hum3 = db.Column(db.String(32))
+    control_mode3 = db.Column(db.String(32))
+    control_method3 = db.Column(db.String(32))
+    temp_status_info3 = db.Column(db.String(32))
+    humi_status_info3 = db.Column(db.String(32))
+
     temp4 = db.Column(db.String(32))
     humidity4 = db.Column(db.String(32))
+    target_temp4 = db.Column(db.String(32))
+    target_hum4 = db.Column(db.String(32))
+    control_temp4 = db.Column(db.String(32))
+    control_hum4 = db.Column(db.String(32))
+    control_mode4 = db.Column(db.String(32))
+    control_method4 = db.Column(db.String(32))
+    temp_status_info4 = db.Column(db.String(32))
+    humi_status_info4 = db.Column(db.String(32))
+
+    temp5 = db.Column(db.String(32))
+    humidity5 = db.Column(db.String(32))
+    target_temp5 = db.Column(db.String(32))
+    target_hum5 = db.Column(db.String(32))
+    control_temp5 = db.Column(db.String(32))
+    control_hum5 = db.Column(db.String(32))
+    control_mode5 = db.Column(db.String(32))
+    control_method5 = db.Column(db.String(32))
+    temp_status_info5 = db.Column(db.String(32))
+    humi_status_info5 = db.Column(db.String(32))
     record_time = db.Column(db.DateTime)
 
 
@@ -108,7 +157,6 @@ def delete_user(user_id):
 def get_inst_data():
     page = request.args.get('page', 1, type=int)
     per_page = 25
- 
     inst_data = InstData.query.order_by(InstData.record_time.desc()).paginate(page=page, per_page=per_page, error_out=False)  
     records_count = InstData.query.count()
     return render_template('inst_data.html', inst_data=inst_data, records_count=records_count)
@@ -235,21 +283,21 @@ def inst_curr_raw_data():
             dashboard_get_info1_dict.update({"temp_" + str(ch) : str(int('0x' + data_list[idx+1] + data_list[idx+2], 16)/10.0) })
             dashboard_get_info1_dict.update({"hum_" + str(ch) : str(int('0x' + data_list[idx+3] + data_list[idx+4], 16)/10.0) })
             if int('0x' + data_list[idx+13], 16) == 1:
-                dashboard_get_info1_dict.update({"control_mode_" + str(ch) : "循环 <i class='glyphicon glyphicon-refresh'></i>" })
+                dashboard_get_info1_dict.update({"control_mode_" + str(ch) : "循环 " })
             else:
-                dashboard_get_info1_dict.update({"control_mode_" + str(ch) : "自动 <i class='glyphicon glyphicon-transfer'></i>" })
+                dashboard_get_info1_dict.update({"control_mode_" + str(ch) : "自动 " })
             
             if int('0x' + data_list[idx+14], 16) == 1:
-                dashboard_get_info1_dict.update({"control_method_" + str(ch) : "节能 <i class='glyphicon glyphicon-leaf'></i>" })
+                dashboard_get_info1_dict.update({"control_method_" + str(ch) : "节能 " })
             else:
-                dashboard_get_info1_dict.update({"control_method_" + str(ch) : "hide" })
+                dashboard_get_info1_dict.update({"control_method_" + str(ch) : " " })
 
             if int('0x' + data_list[idx+15], 16) == 0: #00
                 temp_status_info = ''
             elif int('0x' + data_list[idx+15], 16) == 2: #10
-                temp_status_info = "制冷保护 <i class='glyphicon glyphicon-lock'></i>"
+                temp_status_info = "制冷保护 "
             elif int('0x' + data_list[idx+15], 16) == 1: #01
-                temp_status_info = "加热保护 <i class='glyphicon glyphicon-lock'></i>"
+                temp_status_info = "加热保护 "
             else:
                 temp_status_info = ''
 
@@ -265,19 +313,19 @@ def inst_curr_raw_data():
             # 从低到高每个状态为：加热，制冷，化霜，加湿，抽湿.
             if bin_str_list[7] == '1':
                 # cos_str += "加热 "
-                temp_status_info += "加热 <i class='glyphicon glyphicon-fire'></i>"
+                temp_status_info += "加热 "
             if bin_str_list[6] == '1':
                 # cos_str += "制冷 "
-                temp_status_info += "制冷 <i class='glyphicon glyphicon-asterisk'></i>"
+                temp_status_info += "制冷 "
             if bin_str_list[5] == '1':
                 # cos_str += "化霜 "
                 temp_status_info += "化霜 "
             if bin_str_list[4] == '1':
                 # cos_str += "加湿 "      
-                humi_status_info += "加湿 <i class='glyphicon glyphicon-tint'></i>"  
+                humi_status_info += "加湿 "  
             if bin_str_list[3] == '1':
                 # cos_str += "抽湿 " 
-                humi_status_info += "抽湿 <i class='glyphicon glyphicon-log-out'></i>"
+                humi_status_info += "抽湿 "
             # if cos_str == '':
             #     cos_str = "hide"
             # dashboard_get_info1_dict.update({"control_output_status_" + str(ch) : cos_str })
@@ -311,20 +359,73 @@ def inst_curr_raw_data():
 
     inst_name = "GuanShan Lab"
     sensor_name = "Esp8266"
+
     temp1 = dashboard_get_info1_dict["temp_1"]
     humidity1 = dashboard_get_info1_dict["hum_1"]
+    target_temp1 =  dashboard_get_info1_dict["target_temp_1"]
+    target_hum1 =  dashboard_get_info1_dict["target_hum_1"]
+    control_temp1 =  dashboard_get_info1_dict["control_temp_1"]
+    control_hum1 =  dashboard_get_info1_dict["control_hum_1"]
+    control_mode1 =  dashboard_get_info1_dict["control_mode_1"]
+    control_method1 =  dashboard_get_info1_dict["control_method_1"]
+    temp_status_info1 =  dashboard_get_info1_dict["temp_status_info_1"]
+    humi_status_info1 =  dashboard_get_info1_dict["humi_status_info_1"]
+
     temp2 = dashboard_get_info1_dict["temp_2"]
     humidity2 = dashboard_get_info1_dict["hum_2"]
+    target_temp2 =  dashboard_get_info1_dict["target_temp_2"]
+    target_hum2 =  dashboard_get_info1_dict["target_hum_2"]
+    control_temp2 =  dashboard_get_info1_dict["control_temp_2"]
+    control_hum2 =  dashboard_get_info1_dict["control_hum_2"]
+    control_mode2 =  dashboard_get_info1_dict["control_mode_2"]
+    control_method2 =  dashboard_get_info1_dict["control_method_2"]
+    temp_status_info2 =  dashboard_get_info1_dict["temp_status_info_2"]
+    humi_status_info2 =  dashboard_get_info1_dict["humi_status_info_2"]
+    
     temp3 = dashboard_get_info1_dict["temp_3"]
     humidity3 = dashboard_get_info1_dict["hum_3"]
+    target_temp3 =  dashboard_get_info1_dict["target_temp_3"]
+    target_hum3 =  dashboard_get_info1_dict["target_hum_3"]
+    control_temp3 =  dashboard_get_info1_dict["control_temp_3"]
+    control_hum3 =  dashboard_get_info1_dict["control_hum_3"]
+    control_mode3 =  dashboard_get_info1_dict["control_mode_3"]
+    control_method3 =  dashboard_get_info1_dict["control_method_3"]
+    temp_status_info3 =  dashboard_get_info1_dict["temp_status_info_3"]
+    humi_status_info3 =  dashboard_get_info1_dict["humi_status_info_3"]
+
     temp4 = dashboard_get_info1_dict["temp_4"]
     humidity4 = dashboard_get_info1_dict["hum_4"]
+    target_temp4 =  dashboard_get_info1_dict["target_temp_4"]
+    target_hum4 =  dashboard_get_info1_dict["target_hum_4"]
+    control_temp4 =  dashboard_get_info1_dict["control_temp_4"]
+    control_hum4 =  dashboard_get_info1_dict["control_hum_4"]
+    control_mode4 =  dashboard_get_info1_dict["control_mode_4"]
+    control_method4 =  dashboard_get_info1_dict["control_method_4"]
+    temp_status_info4 =  dashboard_get_info1_dict["temp_status_info_4"]
+    humi_status_info4 =  dashboard_get_info1_dict["humi_status_info_4"]
+
+    temp5 = dashboard_get_info1_dict["temp_5"]
+    humidity5 = dashboard_get_info1_dict["hum_5"]
+    target_temp5 =  dashboard_get_info1_dict["target_temp_5"]
+    target_hum5 =  dashboard_get_info1_dict["target_hum_5"]
+    control_temp5 =  dashboard_get_info1_dict["control_temp_5"]
+    control_hum5 =  dashboard_get_info1_dict["control_hum_5"]
+    control_mode5 =  dashboard_get_info1_dict["control_mode_5"]
+    control_method5 =  dashboard_get_info1_dict["control_method_5"]
+    temp_status_info5 =  dashboard_get_info1_dict["temp_status_info_5"]
+    humi_status_info5 =  dashboard_get_info1_dict["humi_status_info_5"]
+
     record_time = datetime.datetime.now()
 
-    inst_data_item = InstData(inst_name=inst_name, sensor_name=sensor_name, temp1=temp1, humidity1=humidity1, temp2=temp2, humidity2=humidity2, temp3=temp3, humidity3=humidity3, temp4=temp4, humidity4=humidity4, record_time=record_time)
+    inst_data_item = InstData(inst_name=inst_name, sensor_name=sensor_name, 
+                            temp1=temp1, humidity1=humidity1, target_temp1=target_temp1, target_hum1=target_hum1, control_temp1=control_temp1, control_hum1=control_hum1, control_mode1=control_mode1, control_method1=control_method1, temp_status_info1=temp_status_info1, humi_status_info1=humi_status_info1,
+                            temp2=temp2, humidity2=humidity2, target_temp2=target_temp2, target_hum2=target_hum2, control_temp2=control_temp2, control_hum2=control_hum2, control_mode2=control_mode2, control_method2=control_method2, temp_status_info2=temp_status_info2, humi_status_info2=humi_status_info2,                              
+                            temp3=temp3, humidity3=humidity3, target_temp3=target_temp3, target_hum3=target_hum3, control_temp3=control_temp3, control_hum3=control_hum3, control_mode3=control_mode3, control_method3=control_method3, temp_status_info3=temp_status_info3, humi_status_info3=humi_status_info3,
+                            temp4=temp4, humidity4=humidity4, target_temp4=target_temp4, target_hum4=target_hum4, control_temp4=control_temp4, control_hum4=control_hum4, control_mode4=control_mode4, control_method4=control_method4, temp_status_info4=temp_status_info4, humi_status_info4=humi_status_info4,
+                            temp5=temp5, humidity5=humidity5, target_temp5=target_temp5, target_hum5=target_hum5, control_temp5=control_temp5, control_hum5=control_hum5, control_mode5=control_mode5, control_method5=control_method5, temp_status_info5=temp_status_info5, humi_status_info5=humi_status_info5,
+                            record_time=record_time)
     db.session.add(inst_data_item)
     db.session.commit()
-
     return "OK"
 
 
